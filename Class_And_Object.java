2d82c3368caaa41475db5d1a63a66d4a3a0d5db7
Shahadev Karad <shahadevkarad4@gmail.com>
2024-05-30 13:32:59 +0530

    Date 28 / 05 / 24

---
+++ Class_And_Object.java
@@ -0,0 +1,14 @@
+public class Class_And_Object { // <-- This is The Class
+
+    public void method(){
+        System.err.println("I am Method");
+    }
+    public static void main(String[] args) {
+
+        Class_And_Object cla = new Class_And_Object(); // Created Refrence and Object Of Class
+
+        cla.method();  // Calling to the Method Using Object of class
+    }
+}
+
+
